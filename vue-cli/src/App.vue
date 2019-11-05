c224ac7dc2a04f02c3bea47647ca4ebc3d9bc6c3
hjCode1 <gogokye@naver.com>
2019-11-05 23:12:53 +0900

    vue cli 싱글컴포넌트

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    'app-header': AppHeader
   }
 }
 </script>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <header>
+          <h1>This is App-Header</h1>
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
